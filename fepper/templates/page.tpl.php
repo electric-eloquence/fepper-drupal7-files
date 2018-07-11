8eba5828e68c0b67808d59dd8ccfdc7e9b169b27
e2tha-e <ee@electric-eloquence.io>
2018-07-11 16:44:55 -0400

    removing branding block

--- fepper/templates/page.tpl.php
+++ fepper/templates/page.tpl.php
@@ -11,48 +11,45 @@
     <div id="block-fepper-branding">
       <?php if ($logo): ?>
         <?php if ($is_front): ?>
-          <?php if (!$page['branding'] || !$site_name || $title): ?><h1><?php endif; ?>
+          <?php if (!$site_name || $title): ?><h1><?php endif; ?>
         <?php endif; ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
           <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
         </a>
         <?php if ($is_front): ?>
-          <?php if (!$page['branding'] || !$site_name || $title): ?></h1><?php endif; ?>
+          <?php if (!$site_name || $title): ?></h1><?php endif; ?>
         <?php endif; ?>
       <?php endif; ?>
-      <?php
-        // To get the $site_name / $site_slogan to show, add a block to the
-        // Branding region in admin/structure/block.
-      ?>
-      <?php if ($page['branding']): ?>
-        <?php if ($site_name || $site_slogan): ?>
-          <div id="name-and-slogan">
-            <?php if ($site_name): ?>
-              <?php if ($title): ?>
-                <div id="site-name"><strong>
-                  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
-                    <span><?php print $site_name; ?></span>
-                  </a>
-                </strong></div>
-              <?php else: /* Use h1 when the content title is empty */ ?>
-                <h1 id="site-name">
-                  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
-                    <span><?php print $site_name; ?></span>
-                  </a>
-                </h1>
-              <?php endif; ?>
+      <?php if ($site_name || $site_slogan): ?>
+        <div id="name-and-slogan">
+          <?php if ($site_name): ?>
+            <?php if ($title): ?>
+              <div id="site-name"><strong>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
+                  <span><?php print $site_name; ?></span>
+                </a>
+              </strong></div>
+            <?php else: /* Use h1 when the content title is empty */ ?>
+              <h1 id="site-name">
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
+                  <span><?php print $site_name; ?></span>
+                </a>
+              </h1>
             <?php endif; ?>
+          <?php endif; ?>
 
-            <?php if ($site_slogan): ?>
-              <div id="site-slogan"><?php print $site_slogan; ?></div>
-            <?php endif; ?>
-          </div> <!-- /#name-and-slogan -->
-        <?php endif; ?>
-
-        <?php print render($page['branding']); ?>
+          <?php if ($site_slogan): ?>
+            <div id="site-slogan"><?php print $site_slogan; ?></div>
+          <?php endif; ?>
+        </div> <!-- /#name-and-slogan -->
       <?php endif; ?>
     </div> <!-- /#block-fepper-branding -->
 
+    <?php
+      // On a default install, the main-menu expanded will appear in the header.
+      // Do not render this menu if the main-menu block is administratively
+      // enabled in a region at admin/structure/block.
+    ?>
     <?php if (!$main_menu_block_enabled): ?>
       <nav id="main-menu" role="navigation">
         <h2 class="icon-menu">Main menu</h2>
@@ -65,7 +62,7 @@
   </header> <!-- /.header -->
 
   <?php
-    // To get the breadcrumbs to show, add a block to the Breadcrumb region in
+    // To get the breadcrumbs to show, add a block to the Breadcrumb region at
     // admin/structure/block.
   ?>
   <?php if ($page['breadcrumb']): ?>
