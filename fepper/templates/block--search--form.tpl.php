c83882f6319fb252b9064d77082ed9e561f8bd25
e2tha-e <ee@electric-eloquence.io>
2018-07-17 06:57:39 -0400

    mobile nav widgets

--- fepper/templates/block--search--form.tpl.php
+++ fepper/templates/block--search--form.tpl.php
@@ -10,7 +10,10 @@
 
   <?php print render($title_prefix); ?>
   <?php if ($block->subject): ?>
-    <h2 <?php print $title_attributes; ?>><a href="#"><?php print $block->subject ?></a></h2>
+    <h2 <?php print $title_attributes; ?>><?php print $block->subject ?></h2>
+  <?php endif;?>
+  <?php if (!$block->subject): ?>
+    <h2 class="mobile-nav-toggle"><a href="#">Search form</a></h2>
   <?php endif;?>
   <?php print render($title_suffix); ?>
 
