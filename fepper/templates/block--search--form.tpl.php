5188c69dd04e5076f098b5331e61b6d373efe792
e2tha-e <ee@electric-eloquence.io>
2018-07-18 08:58:33 -0400

    style updates

--- fepper/templates/block--search--form.tpl.php
+++ fepper/templates/block--search--form.tpl.php
@@ -2,19 +2,12 @@
 
 /**
  * @file
- * Fepper theme's implementation to display a search form. The difference from
- * the default is the a tag around the subject.
+ * Fepper theme's implementation to display a search form.
  */
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>" <?php print $attributes; ?>>
-
   <?php print render($title_prefix); ?>
-  <?php if ($block->subject): ?>
-    <h2 <?php print $title_attributes; ?>><?php print $block->subject ?></h2>
-  <?php endif;?>
-  <?php if (!$block->subject): ?>
-    <h2 class="mobile-nav-toggle"><a href="#">Search form</a></h2>
-  <?php endif;?>
+  <h2 <?php print $title_attributes; ?>><a href="#"><?php print $block->subject ? $block->subject : 'Search form'; ?></a></h2>
   <?php print render($title_suffix); ?>
 
   <div class="content" <?php print $content_attributes; ?>>
