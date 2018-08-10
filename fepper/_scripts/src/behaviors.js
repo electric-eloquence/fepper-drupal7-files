ff92c0da864b41a0451394025e579409dcbc5dd7
e2tha-e <ee@electric-eloquence.io>
2018-08-10 15:58:06 -0400

    checking for existence of jQuery elements in behaviors.js

--- fepper/_scripts/src/behaviors.js
+++ fepper/_scripts/src/behaviors.js
@@ -9,21 +9,31 @@
   function mobileNavToggle($togglerParent) {
     var $toggler = $togglerParent.find('> h2 > a');
 
-    $toggler.click(function (e) {
-      e.preventDefault();
+    if ($toggler.length) {
+      $toggler.click(function (e) {
+        e.preventDefault();
 
-      $togglerParent.toggleClass('open');
+        $togglerParent.toggleClass('open');
 
-      if (!$togglerParent.hasClass('open')) {
-        $toggler.blur();
-      }
-    });
+        if (!$togglerParent.hasClass('open')) {
+          $toggler.blur();
+        }
+      });
+    }
   }
 
   Drupal.behaviors.openToggle = {
     attach: function (context) {
-      mobileNavToggle($('#main-menu', context));
-      mobileNavToggle($('#block-search-form', context));
+      var searchBlock = $('#block-search-form', context);
+      var mainMenuBlock = $('#main-menu', context);
+
+      if (searchBlock.length) {
+        mobileNavToggle($('#block-search-form', context));
+      }
+
+      if (mainMenuBlock.length) {
+        mobileNavToggle($('#main-menu', context));
+      }
     }
   };
 })(jQuery);
